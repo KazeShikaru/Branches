ea4a9e08c27263a7b300cd9e4896139b5e8d2316
Kaze <renyuan7777@gmail.com>
2020-09-12 01:22:19 -0700

    InitBranchCreation
    First method implemented

---
+++ src/mainPackage/DigitLengthFinder.java
@@ -0,0 +1,25 @@
+package mainPackage;
+
+public class DigitLengthFinder {
+
+	public static void main(String[] args) {
+		System.out.println(findNumberLength(123456789));
+	}
+
+	// Finds the amount of digits in an integer
+	public static int findNumberLength(int inputNumber) {
+		//Spits out 0 if input is 0
+		if(inputNumber == 0) {
+			return 0;
+		}
+		var count = 1;
+		//Loops to count digit
+		while (inputNumber / 10 > 0) {
+			inputNumber = inputNumber/10;
+			count++;
+		}
+		
+		return count;
+	}
+
+}
